47570eef2a587703143a609e35c9ab0f0ceb3fc7
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-05-22 17:32:14 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prog_124_S23_L12_Generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
